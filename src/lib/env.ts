f96765e7ec3c3240f7b5fdd96793e68c183dae8d
Chu Duy <stu735105020@hnue.edu.vn>
2026-08-02 11:14:29 +0700

    fix: fallback to old supabase env names

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -17,8 +17,7 @@ export function parsePublicEnv(input: Record<string, string | undefined>) {
 
 export function getPublicEnv() {
   return parsePublicEnv({
-    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
-    NEXT_PUBLIC_SUPABASE_ANON_KEY:
-      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL || process.env.SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || process.env.SUPABASE_PUBLISHABLE_KEY,
   });
 }
